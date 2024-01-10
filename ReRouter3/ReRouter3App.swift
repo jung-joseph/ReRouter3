3e30446ada689461f219f5d0a45134ed7df54f01
Joseph Jung <joseph.jung@icloud.com>
2024-01-10 07:56:21 -0700

    Initial Commit

---
+++ ReRouter3/ReRouter3App.swift
@@ -0,0 +1,17 @@
+//
+//  ReRouter3App.swift
+//  ReRouter3
+//
+//  Created by Joseph Jung on 1/10/24.
+//
+
+import SwiftUI
+
+@main
+struct ReRouter3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
